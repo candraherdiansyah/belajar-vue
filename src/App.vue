aa94e2d5ab7c240d0afc0306dfe3d8e3e1c23911
candra <candraherdiansyah14@gmail.com>
2023-07-27 09:02:15 +0700

    deploy to vercel

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <navbar-component></navbar-component>
+    <head-component></head-component>
+    <article-component style="padding-top: 20px;"></article-component>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script setup>
+import 'flowbite/dist/flowbite'
+import ArticleComponent from './components/ArticleComponent.vue'
+import HeadComponent from './components/HeadComponent.vue'
+import NavbarComponent from './components/NavbarComponent.vue';
+import FooterComponent from './components/FooterComponent.vue'
+</script>
+
+<style></style>
